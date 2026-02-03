9dd90486f5bbc491c7f891c031184d999d6add31
i-nischal <snischal160@gmail.com>
2026-02-03 10:38:34 +0545

    frontend

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,11 +3,9 @@ import "dotenv/config";
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(
-      `✅ MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold,
-    );
+    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error(`❌ Error: ${error.message}`.red.bold);
+    console.error(`❌ Error: ${error.message}`);
     process.exit(1);
   }
 };
